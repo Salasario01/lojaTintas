cc2b38180a3ca8c33cd43f718ea0378efee31c81
Fellipe Negherbon Prim <99996732+FellipePrim@users.noreply.github.com>
2022-07-12 10:17:45 -0300

    Comit Final

--- php/comprasAndamentoClienteForm.php
+++ php/comprasAndamentoClienteForm.php
@@ -6,20 +6,21 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../style/comprasAndamentoCliente.css">
     <title>comprasAndamentoCliente</title>
 </head>
 <body>
 
 <a href="../index.php">
-<button>Voltar para o catálogo</button>
+<button class="btn btn-success" >Voltar para o catálogo</button>
 </a>
 <a href="../php/carrinhoComprasForm.php">
-<button>Ir para carrinho de compras</button>
+<button class="btn btn-warning">Ir para carrinho de compras</button>
 </a>
 
 <div id="alertas">
--- php/comprasAndamentoFuncionarioForm.php
+++ php/comprasAndamentoFuncionarioForm.php
@@ -12,18 +12,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../style/comprasAndamentoFuncionario.css">
     <title>comprasAndamentoFuncionario</title>
 </head>
 <body>
 
 <a href="../index.php">
-<button>Voltar para o catálogo</button>
+<button id="btn1" >Voltar para o catálogo</button>
 </a>
 <a href="../php/cadastroFuncionarioForm.php">
-<button>Cadastro de Funcionario</button>
+<button id="btn2">Cadastro de Funcionario</button>
 </a>
 <a href="../php/cadastroTintasForm.php">
-<button>Cadastro de Tintas</button>
+<button id="btn3" >Cadastro de Tintas</button>
 </a>
 
 <div id="alertas">
